6bb31fb0397f8e479f5b5f6a458d1fcb5328f669
Hanro <hlareman2@gmail.com>
2022-03-13 14:59:27 +0200

    Update mclc.js

--- modules/mclc.js
+++ modules/mclc.js
@@ -3,8 +3,8 @@ const BE = require("./backEnd");
 function getUUID() {
     var result = ""
     for (var i = 0; i <= 4; i++) {
-        result += (Math.floor(Math.random() * 16777216 )+1048576).toString(16);
-        if (i < 4)result += "-"
+        result += (Math.floor(Math.random() * 16777216) + 1048576).toString(16);
+        if (i < 4) result += "-"
     }
     return result;
 }
@@ -37,64 +37,18 @@ module.exports = {
         return { "name": profile.name, "xuid": profile.meta ? profile.meta.xuid : null, "id": profile.uuid, "_msmc": profile._msmc };
     },
     //Checks if a mclc login object is still valid
-    async validate(profile) {
+    validate(profile) {
         if (profile._msmc) {
             return msmc.validate(self.toProfile(profile));
         }
-        const FETCH = BE.getFetch();
-        const req = {
-            "accessToken": profile.access_token,
-            "clientToken": profile.client_token
-        };
-
-        const r = await FETCH("https://authserver.mojang.com/validate", {
-            "body": JSON.stringify(req),
-            "method": "POST",
-            "headers": {
-                "Content-Type": "application/json"
-            }
-        });
-        return r.status == 204
+        throw "As of the 10th of March 2022. The legacy Mojang authentication endpoints are no longer supported."
     },
 
     async refresh(profile, updates = console.log, authToken) {
-        const FETCH = BE.getFetch();
         if (profile._msmc) {
             return self.getAuth(await msmc.refresh(self.toProfile(profile), updates, authToken));
         } else {
-            updates({ type: "Starting" });
-            updates({ type: "Loading", data: "Refreshing Mojang account", percent: 50 });
-            const req = {
-                "accessToken": profile.access_token,
-                "clientToken": profile.client_token,
-                "requestUser": true
-            };
-
-            const user = await FETCH("https://authserver.mojang.com/refresh", {
-                "body": JSON.stringify(req),
-                "method": "POST",
-                "headers": {
-                    "Content-Type": "application/json"
-                }
-
-            });
-            updates({ type: "Loading", data: "Getting user data", percent: 85 });
-            const data = await user.json();
-
-            if (data.error) {
-                throw data;
-            }
-
-            const userProfile = {
-                access_token: data.accessToken,
-                client_token: data.clientToken,
-                uuid: data.selectedProfile.id,
-                name: data.selectedProfile.name,
-                user_properties: data.user ? data.user.properties : "{}"
-            };
-
-            updates({ type: "Loading", data: "Done!", percent: 100 });
-            return userProfile;
+            throw "As of the 10th of March 2022. The legacy Mojang authentication endpoints are no longer supported."
         }
     }
 }
